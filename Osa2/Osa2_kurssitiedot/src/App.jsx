63dd7585ae4fb600ffa63f508a22c3689280905e
LauriPaloneva <lauri.paloneva@aalto.fi>
2024-02-19 15:01:39 +0200

    erillinen moduuli

--- Osa2/Osa2_kurssitiedot/src/App.jsx
+++ Osa2/Osa2_kurssitiedot/src/App.jsx
@@ -1,35 +1,4 @@
-const Course = ({course}) => {
-
-  const Header = ({name}) => {
-    return (
-        <h1>{name}</h1>
-    )
-  }
-  
-  const Content = ({parts}) => {
-    return (
-  parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)
-    )
-  }
-  
-  const Total = ({exercises}) => {
-    console.log(exercises)
-    const total = exercises.reduce( (s, p) => 
-    s + p)
-    return (<p>total of {total} exercises </p>)
-  }
-  
-  return (
-    <div>
-    <Header name = {course.name}/>
-    <ul> 
-    <Content parts = {course.parts}/>
-    </ul>
-    <Total exercises={course.parts.map(exercise => exercise.exercises)}/>
-    </div>
-  )
-  }
-  
+import Course from './Course'
   
   
   const App = () => {
