2f2daf6de7850db0442effca84d9c0f5c90cb14f
LauriPaloneva <lauri.paloneva@aalto.fi>
2024-02-19 14:54:04 +0200

    kurssitiedot step 6-9

---
+++ Osa2/Osa2_kurssitiedot/src/App.jsx
@@ -0,0 +1,88 @@
+const Course = ({course}) => {
+
+  const Header = ({name}) => {
+    return (
+        <h1>{name}</h1>
+    )
+  }
+  
+  const Content = ({parts}) => {
+    return (
+  parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)
+    )
+  }
+  
+  const Total = ({exercises}) => {
+    console.log(exercises)
+    const total = exercises.reduce( (s, p) => 
+    s + p)
+    return (<p>total of {total} exercises </p>)
+  }
+  
+  return (
+    <div>
+    <Header name = {course.name}/>
+    <ul> 
+    <Content parts = {course.parts}/>
+    </ul>
+    <Total exercises={course.parts.map(exercise => exercise.exercises)}/>
+    </div>
+  )
+  }
+  
+  
+  
+  const App = () => {
+    const courses = [
+      {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
+      {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
+    return (
+      <div>
+        {courses.map(course => <div key = {course.id}><Course course = {course}/></div>)}
+      </div>
+    )
+  }
+  
+  export default App
+  
